b404b94f0736703a604a9aae52375fdae19737a9
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-08 16:50:57 -0500

    annotations

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -1,20 +1,46 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DisplayName("Day 1 Hello Test")
 public class TestHello {
 
+    @BeforeAll
+    public static void setUp(){
+
+        System.out.println("@BeforeAll block");
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("@AfterAll  is running");
+    }
+
+    @BeforeEach
+    public void testSetting(){
+        System.out.println("Before EACH");
+    }
+
+    @AfterEach
+    public void testClosing(){
+        System.out.println("After EACH");
+    }
+@DisplayName("Test num == 3+1")
 @Test
     public void test() {
 
+    System.out.println("Test1 is running");
     int num = 4;
     Assertions.assertEquals(num, 3+1);
 }
 
-
+@Disabled
+@DisplayName("Test hi.equals(Hello)")
 @Test
     public void test2(){
 
+    System.out.println("Test2 is running");
     String hi = "Hello";
     assertEquals(hi, "Hello");
 
