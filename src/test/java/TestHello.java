4420a6ed77f323414f552854075f6765a54b6ea3
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-08 16:14:20 -0500

    initial test

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -1,6 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestHello {
 
-
+@Test
+    public void test() {
+    Assertions.assertEquals(4, 3+1);
+}
 
 
 }
