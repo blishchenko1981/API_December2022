1c0a342b97e95c2f7764e0bd70fd59023dd808bb
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-08 16:29:15 -0500

    Second test

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -1,12 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestHello {
 
 @Test
     public void test() {
-    Assertions.assertEquals(4, 3+1);
+
+    int num = 4;
+    Assertions.assertEquals(num, 3+1);
 }
 
+
+@Test
+    public void test2(){
+
+    String hi = "Hello";
+    assertEquals(hi, "Hello");
+
+
+}
 
 }
