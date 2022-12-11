72a9ecd17f7f8d8e4ea3b926f2bab42a7907c736
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-11 13:41:53 -0500

    Add AfterAll block

--- src/test/java/Day02/SpartanTest.java
+++ src/test/java/Day02/SpartanTest.java
@@ -3,6 +3,7 @@ package Day02;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,11 @@ public class SpartanTest {
         RestAssured.baseURI = "http://54.236.150.168:8000";
         RestAssured.basePath = "/api";
 
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        RestAssured.reset();
     }
 
 
