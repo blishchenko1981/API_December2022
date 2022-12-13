7718897499c0ac1044cfcf9167df5018713dd175
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-13 10:59:47 -0500

    Add Jackson-DataBind

--- src/test/java/Day04/SpartanAddingUpdatingTest.java
+++ src/test/java/Day04/SpartanAddingUpdatingTest.java
@@ -1,13 +1,16 @@
 package Day04;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-
 
 
 //http://54.236.150.168:7000/api/spartans
@@ -21,7 +24,7 @@ public class SpartanAddingUpdatingTest {
     }
 
     @AfterAll
-    public static void tearDown(){
+    public static void tearDown() {
 
         reset();
     }
@@ -29,25 +32,103 @@ public class SpartanAddingUpdatingTest {
     @DisplayName("Get one data TEST with basic Auth")
     @Test
 
-    public void testAdd1data(){
+    public void testGETdataAuth() {
 
 
         given()
                 .auth().basic("admin", "admin")
-                        .log().all().
+                .log().all().
 
-        when()
+                when()
                 .get("/spartans/2").
 
 
-        then()
+                then()
                 .log().body()
                 .statusCode(200);
 
+    }
 
 
+    @DisplayName("POST one data TEST with basic Auth")
+    @Test
 
+    public void testPOSTdataAuth() {
+
+        String newSpartan = "{\n" +
+                "        \"name\": \"Vitaly\",\n" +
+                "        \"gender\": \"Male\",\n" +
+                "        \"phone\": 1234567890\n" +
+                "    }";
+
+
+        given()
+                .log().all()
+                .auth().basic("admin", "admin")
+                .contentType(ContentType.JSON)
+                .body(newSpartan).
+
+
+                when()
+                .post("/spartans").
+
+
+                then()
+                .log().all()
+                .statusCode(is(201))
+                .body("success", is("A Spartan is Born!"))
+                .body("data.name", is("Vitaly"))
+                .body("data.gender", is("Male"))
+        ;
 
 
     }
+
+
+    @DisplayName("POST one data TEST with basic Auth as a MAP Object")
+    @Test
+
+    public void testPOSTdataAuthwithMap() {
+
+        Map<String, Object> mapPayload = new LinkedHashMap<>();
+
+
+        mapPayload.put("name", "Tester");
+        mapPayload.put("gender", "Male");
+        mapPayload.put("phone", 1234543201);
+
+        System.out.println("mapPayload = " + mapPayload);
+
+        given()
+                .auth().basic("admin", "admin")
+                .log().all()
+                .contentType(ContentType.JSON).
+                body(mapPayload).
+        when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201)
+                .contentType(ContentType.JSON)
+                .body("data.name", is("Tester"))
+
+        ;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
