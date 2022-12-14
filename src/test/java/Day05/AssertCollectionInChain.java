3e98d6aa20dc5a17d20d502765b23a7abe271347
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-14 12:20:34 -0500

    add Utility package

---
+++ src/test/java/Day05/AssertCollectionInChain.java
@@ -0,0 +1,69 @@
+package Day05;
+
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class AssertCollectionInChain {
+
+    @BeforeAll
+    public static void setUp() {
+
+        baseURI = "http://54.236.150.168:7000";
+        basePath = "/api";
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+
+        reset();
+    }
+
+
+    @DisplayName("Testing GET /api with basic Auth")
+    @Test
+
+    public void testSearchAndExtractData(){
+
+        // search for nameContains : li, gender = Female
+        // extract jsonPath object to get list of all results
+        // check the size of result
+        // verify all names from result contains "li"
+        given()
+                .auth().basic("admin", "admin")
+                .queryParam("nameContains", "li")
+                .queryParam("gender", "Female").
+        when()
+                .get("/spartans/search").
+        then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("numberOfElements", equalTo(11))
+                .body("content", hasSize(11))
+                .body("content.name", everyItem(containsString("li")))
+                .body("content.gender", everyItem(is("Female")))
+                ;
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
