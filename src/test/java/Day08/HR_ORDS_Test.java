4126ebe100a59d197c320b615e817bd08150bb9d
blishchenko1981 <blishchenko1981@gmail.com>
2022-12-19 11:18:28 -0500

    Create TEstBAse

--- src/test/java/Day08/HR_ORDS_Test.java
+++ src/test/java/Day08/HR_ORDS_Test.java
@@ -1,25 +1,52 @@
 package Day08;
 
+import POJO.Country;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import testBase.HR_ORDS_TestBase;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
-public class HR_ORDS_Test {
-
-    @BeforeAll
-    public static void setUp() {
+public class HR_ORDS_Test extends HR_ORDS_TestBase {
 
 
-        baseURI = "http://54.236.150.168:1000";
-        basePath = "/ords/hr/";
+
+    @DisplayName("Test GET /countries/ {country_id} to POJO")
+    @Test
+    public void testCountryResponseToPOJO(){
+
+        Response response = given()
+                .pathParam("country_id", "AR").
+                when()
+                .get("countries/{country_id}").prettyPeek();
+
+
+        Country argent = response.as(Country.class);
+
+        System.out.println("argent = " + argent);
+
+        Country ar1 = response.jsonPath().getObject("", Country.class);
+
+        System.out.println("ar1 = " + ar1);
+
 
     }
 
-    @AfterAll
-    public static void tearDown() {
+    @DisplayName("Test GET /countries to  list of POJO")
+    @Test
+    public void testCountryResponseToListOfPOJO() {
+        Response response = get("/countries");
 
-        reset();
+        List<Country> countryList = response.jsonPath().getList("items");
+
+        System.out.println("countryList = " + countryList);
+
     }
+
 
 }
---
+++ src/test/java/testBase/HR_ORDS_TestBase.java
@@ -0,0 +1,25 @@
+package testBase;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.*;
+
+public class HR_ORDS_TestBase {
+
+    @BeforeAll
+    public static void setUp() {
+
+
+        baseURI = "http://54.236.150.168:1000";
+        basePath = "/ords/hr/";
+
+    }
+
+    @AfterAll
+    public static void tearDown() {
+
+        reset();
+    }
+
+}
