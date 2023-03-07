c11ee279373440e718ea800b0e809d00e1790a42
blishchenko1981 <blishchenko1981@gmail.com>
2023-03-07 09:46:41 -0500

    March2023

--- src/test/java/Day08/ORDS_API_DB_Test.java
+++ src/test/java/Day08/ORDS_API_DB_Test.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import testBase.HR_ORDS_TestBase;
 import utility.DB_Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -42,11 +44,11 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
         System.out.println("r3 = " + r3);
 
         DB_Util.runQuery("select * from regions where region_id = " + myID);
-        Map<String , String> expectedResultMap = DB_Util.getRowMap(1);
+        Map<String, String> expectedResultMap = DB_Util.getRowMap(1);
 
         System.out.println("expectedResult from DB " + expectedResultMap);
 
-        assertThat( r3.getRegion_id() + "", is(expectedResultMap.get("REGION_ID")));
+        assertThat(r3.getRegion_id() + "", is(expectedResultMap.get("REGION_ID")));
     }
 
     @DisplayName("Testing GET / regions/{region_id} Data match database with both Maps")
@@ -63,7 +65,7 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
                 .statusCode(200)
                 .extract().response();
 
-        Map<String ,String> resultFromAPI = response.jsonPath().getMap("");
+        Map<String, String> resultFromAPI = response.jsonPath().getMap("");
 
         System.out.println("resultFromAPI = " + resultFromAPI);
 
@@ -72,7 +74,7 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
 
 
         DB_Util.runQuery("select * from regions where region_id = " + myID);
-        Map<String , String> expectedResultMapFromDB = DB_Util.getRowMap(1);
+        Map<String, String> expectedResultMapFromDB = DB_Util.getRowMap(1);
         System.out.println("expectedResultMapFromDB = " + expectedResultMapFromDB);
 
         assertThat(expectedResultMapFromDB.get("region_name"), equalTo(resultFromAPI.get("REGION_NAME")));
@@ -89,22 +91,72 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBase {
                 = given()
                 .pathParam("region_id", regID).
                 when()
-                        .get("/regions/{region_id}").
-                        then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().response()
-                ;
+                .get("/regions/{region_id}").
+                then()
+                .log().all()
+                .statusCode(200)
+                .extract().response();
 
         String regionID_api = response.jsonPath().getString("region_id");
         String regionName_api = response.jsonPath().getString("region_name");
 
         DB_Util.runQuery("select * from regions where region_id = " + regID);
-       String regionName_DB =  DB_Util.getCellValue(1, "REGION_NAME");
-       String regionID_DB = DB_Util.getCellValue(1, "REGION_ID");
+        String regionName_DB = DB_Util.getCellValue(1, "REGION_NAME");
+        String regionID_DB = DB_Util.getCellValue(1, "REGION_ID");
 
-                assertThat(regionID_api, equalTo(regionID_DB));
-                assertThat(regionName_api, equalTo(regionName_DB));
+        assertThat(regionID_api, equalTo(regionID_DB));
+        assertThat(regionName_api, equalTo(regionName_DB));
+
+    }
+
+
+    @Test
+    public void orderArrayAsc() {
+        int[] nums = {1,3,4,5, -1 ,0,2,-100, -200, 100 , 200  , 20000};
+
+
+        for (int i = 0; i < nums.length -1; i++) {
+
+
+            for (int j = i+1; j < nums.length; j++) {
+
+                int temp = nums[i];
+                if(nums[j]< nums[i]){
+
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+        System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
+
+
+
+
+        for (int i = 0 ; i< nums.length-1; i++){
+
+
+            for (int j = i+1; j < nums.length; j++) {
+                int temp = nums[i];
+                if(nums[i]>nums[j]) {
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+
+                }
+            }
+
+        }
+
+        System.out.println("nums = " + Arrays.toString(nums));
+
 
     }
 }
